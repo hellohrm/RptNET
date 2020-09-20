ad95b849460cf6119974138046efd854ae81509f
caicangcua <36462470+caicangcua@users.noreply.github.com>
2020-09-20 22:10:51 +0700

    v1

---
+++ ReportingWebApp/PredefinedReports/TestReport.cs
@@ -0,0 +1,13 @@
+using System;
+using DevExpress.XtraReports.UI;
+
+namespace ReportingWebApp.PredefinedReports
+{
+    public partial class TestReport
+    {
+        public TestReport()
+        {
+            InitializeComponent();
+        }
+    }
+}
